dd7528e736ce328a60849cfe6874ace980fb8547
MessumAli <messum.ali.72@gmail.com>
2023-12-31 21:35:01 +0500

    fixed vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'https://mern-todo-app-backend.vercel.app/',
+      '/api': 'https://mern-todo-app-backend.vercel.app',
     },
   },
 })
